7450e595bdac4295a10c11e484a12b018363a557
안현석 <ajy720@gmail.com>
2020-01-15 23:22:29 +0900

    Add Major Subject

--- src/components/SelectOptions/SubjectOption/MajorSubjectOption.tsx
+++ src/components/SelectOptions/SubjectOption/MajorSubjectOption.tsx
@@ -5,6 +5,9 @@ const MajorSubjectOption: React.FC = () => {
         <>
             <option value="7">프로그래밍</option>
             <option value="8">컴퓨터 구조</option>
+            <option value="9">컴퓨터 시스템 일반</option>
+            <option value="10">자료 구조</option>
+            <option value="11">컴퓨터 네트워크</option>
         </>
     );
 };
